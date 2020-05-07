6b8893ae8391913d2eb57f459c8aaf63513361d3
buchidiai <31743169+buchidiai@users.noreply.github.com>
2020-05-07 13:16:21 -0700

    array

---
+++ src/com/datastructure/OneDimensionArray.java
@@ -0,0 +1,39 @@
+package com.datastructure;
+
+public class OneDimensionArray {
+    //Arrays
+    public static void main(String[] args) {
+
+        //create array size of 5
+        int[] numbers = new int[5];
+        numbers[0] = 20;
+        numbers[1] = 5;
+        numbers[2] = 10;
+        numbers[3] = 6;
+        numbers[4] = 11;
+
+
+        //print array
+        for (int i = 0; i < numbers.length; i++) {
+
+            System.out.println(numbers[i]);
+        }
+
+        //array of student objects
+        Student[] students = new Student[4];
+        students[0] = new Student("Mike", 27);
+        students[1] = new Student("Chris", 9);
+        students[2] = new Student("Tim", 3);
+        students[3] = new Student("Rob", 30);
+
+        //print array of students
+        for (Student student : students) {
+            System.out.println("name : " + student.getName() +
+                    ", Age :" + student.getAge());
+        }
+
+
+    }
+
+
+}
---
+++ src/com/datastructure/Student.java
@@ -0,0 +1,19 @@
+package com.datastructure;
+
+public class Student {
+    private final String name;
+    private final int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+}
